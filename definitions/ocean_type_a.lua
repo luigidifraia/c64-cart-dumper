41c76abd4e8f7ae0b5ffd71a86b9078168e1dedc
Luigi Di Fraia <luigidifraia@users.noreply.github.com>
2020-02-09 01:22:27 +0000

    Refer to this format as "dual chip"

--- definitions/ocean_type_a.lua
+++ definitions/ocean_type_a.lua
@@ -1,10 +1,9 @@
--- Ocean type A dumping definition file (all Ocean cartridges excluding "Chase H.Q. II" and "Terminator 2")
+-- Ocean type A (dual chip) dumping definition file ("Robocop 2", "Shadow of the Beast", "Space Gun")
 -- for the Commodore 64 Cartridge Dumper client
 -- (C) 2019 Luigi Di Fraia
 
 -- Bank selection circuitry uses:
---  128kB cartridges (all 128kB titles): bits 0-3 at $DE00 and ROML (one 128kB chip, with A16 on pin 22, rather than the usual /OE signal)
---  256kB cartridges ("Robocop 2", "Shadow of the Beast", "Space Gun"): bits 0-3 at $DE00 and ROML/H (two 128kB chips)
+--  256kB cartridges: bits 0-3 at $DE00 and ROML/H (two 128kB chips)
 
 -- Calculate the number of 8kB banks to dump
 local banks = size_kb / 8
