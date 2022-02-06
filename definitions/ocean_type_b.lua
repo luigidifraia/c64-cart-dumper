41bb2dcdc563c76d8d1ee8ee0969bf2225d0c976
Luigi Di Fraia <luigidifraia@gmail.com>
2022-02-06 09:03:48 +0000

    Minor rewording

--- definitions/ocean_type_b.lua
+++ definitions/ocean_type_b.lua
@@ -1,4 +1,4 @@
--- Ocean type B (single chip) dumping definition file
+-- Ocean type B (one memory chip) dumping definition file
 -- for the Commodore 64 Cartridge Dumper client
 -- (C) 2019-2021 Luigi Di Fraia
 
@@ -6,9 +6,9 @@
 --  All Ocean titles excluding "Robocop 2" and "Shadow of the Beast"
 
 -- Bank selection circuitry uses:
---  128 KiB cartridges (all known titles): bits 0-3 at $DE00 and /ROML (single 128 KiB chip with A16 on pin 22, rather than the /OE signal)
---  256 KiB cartridges (just "Chase H.Q. II"): bits 0-4 at $DE00 and /ROML (single 256 KiB chip)
---  512 KiB cartridges (just "Terminator 2"):  bits 0-5 at $DE00 and /ROML (single 512 KiB chip)
+--  128 KiB cartridges (all known titles): bits 0-3 at $DE00 and /ROML (one 128 KiB chip with A16 on pin 22, rather than the /OE signal)
+--  256 KiB cartridges (just "Chase H.Q. II"): bits 0-4 at $DE00 and /ROML (one 256 KiB chip)
+--  512 KiB cartridges (just "Terminator 2"):  bits 0-5 at $DE00 and /ROML (one 512 KiB chip)
 
 -- Calculate the number of 8 KiB banks to dump
 local banks = size_kb / 8
