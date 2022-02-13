1a30ef0e6ec3d739eb526dc912cefd44eec54540
Luigi Di Fraia <luigidifraia@users.noreply.github.com>
2022-02-13 22:51:30 +0000

    Rationalise bank switching comments (#6)

--- definitions/ocean_type_a.lua
+++ definitions/ocean_type_a.lua
@@ -1,4 +1,4 @@
--- Ocean Software Type A (two memory chips) dumping definition file
+-- Ocean Software Type A ($8000-$9FFF and $A000-$BFFF) dumping definition file
 -- for the Commodore 64 Cartridge Dumper client
 -- (C) 2019-2022 Luigi Di Fraia
 
@@ -7,7 +7,17 @@
 --  - "Shadow of the Beast" (tested)
 
 -- Bank selection circuitry uses:
---  256 KiB cartridges (all known titles): bits 0-3 at $DE00, /ROML, and /ROMH (two 128 KiB chips)
+--  256 KiB cartridges (all known titles): data bus bits 0-3, /IO1, /ROML, and /ROMH (two 128 KiB chips)
+
+-- As an example, "Robocop 2" uses two 128 KiB 28-pin mask ROM chips:
+--  - for both, A13 is set to the output of a 75LS273 that latches bit 0 of the data bus
+--  - for both, A14 is set to the output of the same 75LS273 that latches bit 1 of the data bus
+--  - for both, A15 is set to the output of the same 75LS273 that latches bit 2 of the data bus
+--  - for both, A16 is set to the output of the same 75LS273 that latches bit 3 of the data bus
+--  - for one chip (lower 128 KiB), /CE (Chip Enable) is connected /ROML
+--  - for the other chip (upper 128 KiB), /CE is connected to /ROMH
+--  - latching occurs when /IO1 is asserted (i.e. the value on the address bus is in range $DE00-$DEFF)
+
 
 -- Calculate the number of 8 KiB banks to dump
 local banks = size_kb / 8
--- definitions/ocean_type_b.lua
+++ definitions/ocean_type_b.lua
@@ -1,14 +1,22 @@
--- Ocean Software Type B (one memory chip) dumping definition file
+-- Ocean Software Type B ($8000-$9FFF) dumping definition file
 -- for the Commodore 64 Cartridge Dumper client
 -- (C) 2019-2022 Luigi Di Fraia
 
 -- Supported titles:
---  All titles, excluding "Robocop 2" and "Shadow of the Beast"
+--  All Ocean Software titles, excluding "Robocop 2" and "Shadow of the Beast"
 
 -- Bank selection circuitry uses:
---  128 KiB cartridges (all known titles): bits 0-3 at $DE00 and /ROML (one 128 KiB chip with A16 on pin 22, rather than the /OE signal)
---  256 KiB cartridges (just "Chase H.Q. II"): bits 0-4 at $DE00 and /ROML (one 256 KiB chip)
---  512 KiB cartridges (just "Terminator 2"):  bits 0-5 at $DE00 and /ROML (one 512 KiB chip)
+--  128 KiB cartridges (all known titles):     data bus bits 0-3, /IO1, and /ROML (one 128 KiB chip)
+--  256 KiB cartridges (just "Chase H.Q. II"): data bus bits 0-4, /IO1, and /ROML (one 256 KiB chip)
+--  512 KiB cartridges (just "Terminator 2"):  data bus bits 0-5, /IO1, and /ROML (one 512 KiB chip)
+
+-- As an example, "Navy Seals" uses a single 128 KiB 28-pin mask ROM chip (A16 on pin 22, rather than the /OE signal):
+--  - /CE (Chip Enable) is connected to /ROML,
+--  - A13 is set to the output of a 75LS273 that latches bit 0 of the data bus
+--  - A14 is set to the output of the same 75LS273 that latches bit 1 of the data bus
+--  - A15 is set to the output of the same 75LS273 that latches bit 2 of the data bus
+--  - A16 is set to the output of the same 75LS273 that latches bit 3 of the data bus
+--  - latching occurs when /IO1 is asserted (i.e. the value on the address bus is in range $DE00-$DEFF)
 
 -- Calculate the number of 8 KiB banks to dump
 local banks = size_kb / 8
