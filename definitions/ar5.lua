ad44ea04151e9f1be841ba47357bbd798d5e1ae9
Luigi Di Fraia <luigidifraia@users.noreply.github.com>
2022-02-13 22:51:30 +0000

    Rationalise bank switching comments (#6)

--- definitions/ar5.lua
+++ definitions/ar5.lua
@@ -1,9 +1,9 @@
--- Action Replay 4.x, 5, and 6 dumping definition file
+-- Action Replay 4.x, 5, and 6 ($8000-$9FFF) dumping definition file
 -- for the Commodore 64 Cartridge Dumper client
--- (C) 2019-2021 Luigi Di Fraia
+-- (C) 2019-2022 Luigi Di Fraia
 
 -- Bank selection circuitry uses:
---  Bits 3 and 4 at $DE00 are used to set address lines A13 and A14 of the memory chip
+--  Data bus bits 3 and 4, latched to set A13 and A14 respectively, and /IO1
 
 -- 4 banks of 8 KiB each
 local banks = 4
