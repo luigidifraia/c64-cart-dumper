d41e787ff0d859badcf6c08edf30992b69ffd6d9
Luigi Di Fraia <luigidifraia@gmail.com>
2022-02-20 14:45:23 +0000

    Consolidate bank switching info into the README.md file

--- definitions/dinamic.lua
+++ definitions/dinamic.lua
@@ -5,18 +5,6 @@
 -- Supported titles:
 --  All Dinamic Software titles
 
--- Bank selection circuitry uses:
---  128 KiB cartridges (all known titles): address bus bits 0-3, /IO1, and /ROML
-
--- As an example, "Astro Marine Corps" uses two 64 KiB 28-pin EPROM chips in a piggyback configuration:
---  - for both, /OE (Output Enable) is connected to /ROML,
---  - for both, A13 is set to the output of a 75LS175 that latches bit 0 of the address bus
---  - for both, A14 is set to the output of the same 75LS175 that latches bit 1 of the address bus
---  - for both, A15 is set to the output of the same 75LS175 that latches bit 2 of the address bus
---  - for one chip (lower 64 KiB), /CE (Chip Enable) is set to the output of the same 75LS175 that latches bit 3 of the address bus
---  - for the other chip (upper 64 KiB), /CE is connected to the inverted output of the same 75LS175 that latches bit 3 of the address bus
---  - latching occurs when /IO1 is asserted (i.e. the value on the address bus is in range $DE00-$DEFF) and PHI2 is asserted
-
 -- Calculate the number of 8 KiB banks to dump
 local banks = size_kb / 8
 local b = 0
--- definitions/ocean_type_b.lua
+++ definitions/ocean_type_b.lua
@@ -5,19 +5,6 @@
 -- Supported titles:
 --  All Ocean Software titles, excluding "Robocop 2" and "Shadow of the Beast"
 
--- Bank selection circuitry uses:
---  128 KiB cartridges (all known titles):     data bus bits 0-3, /IO1, and /ROML (one 128 KiB chip)
---  256 KiB cartridges (just "Chase H.Q. II"): data bus bits 0-4, /IO1, and /ROML (one 256 KiB chip)
---  512 KiB cartridges (just "Terminator 2"):  data bus bits 0-5, /IO1, and /ROML (one 512 KiB chip)
-
--- As an example, "Navy Seals" uses a single 128 KiB 28-pin mask ROM chip (A16 on pin 22, rather than the /OE signal):
---  - /CE (Chip Enable) is connected to /ROML,
---  - A13 is set to the output of a 75LS273 that latches bit 0 of the data bus
---  - A14 is set to the output of the same 75LS273 that latches bit 1 of the data bus
---  - A15 is set to the output of the same 75LS273 that latches bit 2 of the data bus
---  - A16 is set to the output of the same 75LS273 that latches bit 3 of the data bus
---  - latching occurs when /IO1 is asserted (i.e. the value on the address bus is in range $DE00-$DEFF) and PHI2 is asserted
-
 -- Calculate the number of 8 KiB banks to dump
 local banks = size_kb / 8
 local b = 0
