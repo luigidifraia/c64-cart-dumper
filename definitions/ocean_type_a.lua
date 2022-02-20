a64e8f3cd94b7973e1d81e0e2bfc0faf3530fe81
Luigi Di Fraia <luigidifraia@users.noreply.github.com>
2022-02-20 14:46:05 +0000

    Consolidate bank switching info into the README.md file (#10)

--- definitions/ocean_type_a.lua
+++ definitions/ocean_type_a.lua
@@ -6,18 +6,6 @@
 --  - "Robocop 2" (tested)
 --  - "Shadow of the Beast" (tested)
 
--- Bank selection circuitry uses:
---  256 KiB cartridges (all known titles): data bus bits 0-3, /IO1, /ROML, and /ROMH (two 128 KiB chips)
-
--- As an example, "Robocop 2" uses two 128 KiB 28-pin mask ROM chips:
---  - for both, A13 is set to the output of a 75LS273 that latches bit 0 of the data bus
---  - for both, A14 is set to the output of the same 75LS273 that latches bit 1 of the data bus
---  - for both, A15 is set to the output of the same 75LS273 that latches bit 2 of the data bus
---  - for both, A16 is set to the output of the same 75LS273 that latches bit 3 of the data bus
---  - for one chip (lower 128 KiB), /CE (Chip Enable) is connected /ROML
---  - for the other chip (upper 128 KiB), /CE is connected to /ROMH
---  - latching occurs when /IO1 is asserted (i.e. the value on the address bus is in range $DE00-$DEFF) and PHI2 is asserted
-
 -- Calculate the number of 8 KiB banks to dump
 local banks = size_kb / 8
 local hstart = banks / 2
